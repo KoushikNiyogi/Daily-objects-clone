aecca8f0e09b6c0035e84d2e216d90fa88559f57
Koushik Niyogi <112813924+KoushikNiyogi@users.noreply.github.com>
2023-05-04 23:14:33 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
 import Navbar from "./Components/Navbar"
 import Footer from './Components/Footer';
-function App() {
-  return (
-    <div>
-       
-
 import MainRoutes from './Pages/MainRoutes';
-// import Address from './Components/Address'
-// import ShoppingBag from './Components/ShoppingBag'
+
 
 
 function App() {
@@ -18,9 +11,6 @@ function App() {
     <div className="App">
 
       <MainRoutes/>
-
-      {/* <Address /> */}
-      {/* <ShoppingBag /> */}
 
     </div>
   );
