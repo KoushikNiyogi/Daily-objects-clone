7492ed5bd4aba42880ae44d517e526300a0c4ef1
NehaP0 <nehaphadtare334@gmail.com>
2023-05-04 22:50:09 +0530

    Neha Day2 Address Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import MainRoutes from './Pages/MainRoutes';
+// import Address from './Components/Address'
+// import ShoppingBag from './Components/ShoppingBag'
+
 
 function App() {
   return (
     <div className="App">
+
       <MainRoutes/>
+{/* comment below */}
+      {/* <Address /> */}
+      {/* <ShoppingBag /> */}
     </div>
   );
 }
