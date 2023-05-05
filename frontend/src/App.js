ebbe04f068719ba43c1ab97903d4df30c949e57f
Koushik niyogi <koushikniyogionlinework@gmail.com>
2023-05-06 01:17:08 +0530

    initial commit of this branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import logo from "./logo.svg";
 import "./App.css";
-
 import Footer from "./Components/Footer";
 import MainRoutes from "./Pages/MainRoutes";
 import AdminAllroutes from "./Components/Admin/Routes/Admin.routes";
@@ -8,7 +7,6 @@ import AdminAllroutes from "./Components/Admin/Routes/Admin.routes";
 function App() {
   return (
     <div className="App">
-      <AdminAllroutes />
       <MainRoutes />
     </div>
   );
