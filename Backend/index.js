e79feaa3309e21f1361052331b5f8ba7d58efe60
Rajesh Pradhan <pradhan143rajesh@gmail.com>
2023-05-06 23:15:09 +0530

    day-4

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const connection = require("./config/db");
 const cors = require("cors");
-const userRouter = require("./routes/user.route")
-const productRouter = require("./routes/product.route")
+const userRouter = require("./routes/user.route");
+const productRouter = require("./routes/product.route");
 const adminRouter = require("./routes/admin.route");
 const cartRoute = require("./routes/cart.route");
 const { Logger } = require("./middlewares/logger");
@@ -14,14 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/user", userRouter)
-app.use("/admin",adminRouter)
-app.use("/product", productRouter)
-app.use("/wishlist",wishlistRoute)
-// app.use(auth)
-// app.use(Logger)
-app.use("/cart",cartRoute)
-
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/product", productRouter);
+app.use(auth);
+app.use(Logger);
+app.use("/cart", cartRoute);
+app.use("/wishlist", wishlistRoute);
 app.get("/", (req, res) => {
   res.status(200).send("Home page");
 });
@@ -31,7 +30,7 @@ app.listen(process.env.port, async () => {
     await connection;
     console.log("Connected to Mongo Atlas");
   } catch (err) {
-    console.log(err)
+    console.log(err);
     console.log("Couldn't connect to Mongo Atlas");
   }
   console.log(`Server started on port ${process.env.port}`);
