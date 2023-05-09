0821bfb667048260d743958eef66054a8c1c260f
Prashant Sathawane <prashantsathawane2099@gmail.com>
2023-05-09 17:33:48 +0530

    category responsive

--- frontend/src/Components/ProductCard.jsx
+++ frontend/src/Components/ProductCard.jsx
@@ -9,19 +9,29 @@ import {
     Text,
     Stack,
     Image,
+    useToast,
   } from '@chakra-ui/react';
   import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {addToWishlist} from "../Redux/WishlistReducer/action"
+// import "../Pages/Styles/ProductCardCss"
   
   export const ProductCard=({_id,images,item ,title , price, discounted_price,offer })=> {
     const {isAuth,token} = useSelector(store => store.Loginreducer)
+    // const image1 = item.images[0];
+    // const image2 = item.images[1];
+    // const [image, setImage] = React.useState(image1);
     const Navigate = useNavigate();
     const dispatch = useDispatch();  
+    const toast = useToast()
     
     const handleAddToWishlist = (item) => {
+      let obj = {...item}
+      obj["productId"] = item["_id"];
+      console.log(obj);
+      delete obj["_id"];
       if(token){
-       dispatch(addToWishlist(token,item))
+       dispatch(addToWishlist(token,obj,item,toast))
       }else{
         Navigate("/login",{replace:true,state:"/categorypage"})
       }
@@ -77,6 +87,8 @@ import {addToWishlist} from "../Redux/WishlistReducer/action"
               width={"100%"}
               objectFit={'cover'}
               src={images}
+              // onMouseEnter={() => setImage(image2)}
+              // onMouseOut={() => setImage(image1)}
             />
              
           </Box>
@@ -104,69 +116,3 @@ import {addToWishlist} from "../Redux/WishlistReducer/action"
       </Center>
     );
   }
-
-
-
-
-// import React from 'react'
-// import { AiOutlineHeart } from "react-icons/ai";
-// import {
-//   Flex,
-//   Box,
-//   Image,
-//   Stack,
-//   Heading,
-//   Text,
-//   Card,
-//   CardBody
-// } from "@chakra-ui/react"
-// import { useNavigate } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {addToWishlist} from "../Redux/WishlistReducer/action"
-
-// const ProductCard = ({ item }) => {
-//   const {isAuth,token} = useSelector(store => store.Loginreducer)
-//   // const image1 = item.images[0];
-//   // const image2 = item.images[1];
-//   const [image, setImage] = React.useState(item.images);
-//   const Navigate = useNavigate();
-//   const dispatch = useDispatch();  
-
-//   const handleAddToWishlist = (item) => {
-//     if(token){
-//      dispatch(addToWishlist(token,item))
-//     }else{
-//       Navigate("/login",{replace:true,state:"/categorypage"})
-//     }
-//   }
-
-//   return (
-//     <Card maxW='sm' onClick={() =>  Navigate(`/products/${item.id}`)}>
-//       <CardBody>
-//         <Flex justifyContent={"flex-end"} padding={"10px"}><AiOutlineHeart className ='button' onClick={(e) => {
-//           e.stopPropagation();
-//           handleAddToWishlist(item);
-//         }} size={"25px"} /></Flex>
-
-//         <Box>
-//           <Image
-//             boxSize={"400px"}
-//             objectFit='cover'
-//             src={item.images}
-//             // onMouseEnter={() => setImage(image2)}
-//             // onMouseOut={() => setImage(image1)}
-//           />
-//         </Box>
-//         <Stack mt='6' spacing='3'>
-//           <Text fontSize={"sm"}>
-//             {item.title}
-//           </Text>
-//           <Flex><Text fontSize={"md"} color={"black"}>Rs.{item.price}</Text> <Text ml={"10px"} textDecoration={"line-through"} fontSize={"sm"}>{item.discounted_price}</Text></Flex>
-//           item.offer!=undefined&&<Text fontSize={"md"} color={"red"}>{item.offer}</Text>
-//         </Stack>
-//       </CardBody>
-//     </Card>
-//   )
-// }
-
-// export default ProductCard
